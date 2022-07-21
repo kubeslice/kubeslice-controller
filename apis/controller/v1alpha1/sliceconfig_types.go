27edc944fa3ae11fbe67e36c57aa906836a86ce7
MD IMRAN <imranaec@outlook.com>
2022-07-21 15:21:52 +0530

    feat(AM-4426): fix maxcluster

--- apis/controller/v1alpha1/sliceconfig_types.go
+++ apis/controller/v1alpha1/sliceconfig_types.go
@@ -38,10 +38,10 @@ type SliceConfigSpec struct {
 	QosProfileDetails         *QOSProfile               `json:"qosProfileDetails,omitempty"` // FIXME: Add OneOf StandardQosProfileName vs QosProfileDetails
 	NamespaceIsolationProfile NamespaceIsolationProfile `json:"namespaceIsolationProfile,omitempty"`
 	ExternalGatewayConfig     []ExternalGatewayConfig   `json:"externalGatewayConfig,omitempty"`
-	//+kubebuilder:default:=16
 	//+kubebuilder:validation:Minimum=2
 	//+kubebuilder:validation:Maximum=32
-	MaxClusters int `json:"maxClusters"`
+	//+kubebuilder:default:=16
+	MaxClusters int `json:"maxClusters,omitempty"`
 }
 
 // ExternalGatewayConfig is the configuration for external gateways like 'istio', etc/
