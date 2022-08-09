44ecb6ddcb67a354557a3f26490783c8da751ba6
Nishant Singh <89241026+NishantSingh10@users.noreply.github.com>
2022-08-09 22:19:41 +0530

    Update start-kind.sh

--- .github/workflows/scripts/start-kind.sh
+++ .github/workflows/scripts/start-kind.sh
@@ -26,7 +26,7 @@ if [ ! -f profile/kind.yaml ];then
   IP1=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
   IP2=$(docker inspect worker-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
 
-echo "${userent}"
+echo ${userent}
 
   cat > profile/kind.yaml << EOF
 Kubeconfig: kinde2e.yaml
