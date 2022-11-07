16216bfe8b63fdb6347a99d9e77dec69c43e8e40
Nishant Singh <89241026+NishantSingh10@users.noreply.github.com>
2022-11-07 18:40:57 +0530

    updated load image
    Signed-off-by: Nishant Singh <89241026+NishantSingh10@users.noreply.github.com>

--- .github/workflows/scripts/start-kind.sh
+++ .github/workflows/scripts/start-kind.sh
@@ -4,10 +4,10 @@
 if [ ! $(kind get clusters | grep controller) ];then
   kind create cluster --name controller --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.22.7
   ip=$(docker inspect controller-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress') 
-#  echo $ip
-# loading docker image into kind controller
-   kind load docker-image kubeslice-controller:e2e-latest --name controller
-# Replace loopback IP with docker ip
+  #  echo $ip
+  # loading docker image into kind controller
+  kind load docker-image kubeslice-controller:${GITHUB_HEAD_COMMIT} --name controller
+  # Replace loopback IP with docker ip
   kind get kubeconfig --name controller | sed "s/127.0.0.1.*/$ip:6443/g" > /home/runner/.kube/kind1.yaml
 fi
 
@@ -15,10 +15,10 @@ fi
 if [ ! $(kind get clusters | grep worker) ];then
   kind create cluster --name worker --config .github/workflows/scripts/cluster.yaml --image kindest/node:v1.22.7
   ip=$(docker inspect worker-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
-#  echo $ip
-# loading docker image into kind controller
-   kind load docker-image kubeslice-controller:e2e-latest --name worker
-# Replace loopback IP with docker ip
+  #  echo $ip
+  # loading docker image into kind controller
+  kind load docker-image kubeslice-controller:${GITHUB_HEAD_COMMIT} --name worker
+  # Replace loopback IP with docker ip
   kind get kubeconfig --name worker | sed "s/127.0.0.1.*/$ip:6443/g" > /home/runner/.kube/kind2.yaml
 fi
 
