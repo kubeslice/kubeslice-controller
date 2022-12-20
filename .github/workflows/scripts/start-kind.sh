ab9b314ef73b7c13b8774de6816ad05c08add9ef
Nishant Singh <89241026+NishantSingh10@users.noreply.github.com>
2022-12-20 08:08:50 +0530

    updated stable chart
    Signed-off-by: Nishant Singh <89241026+NishantSingh10@users.noreply.github.com>

--- .github/workflows/scripts/start-kind.sh
+++ .github/workflows/scripts/start-kind.sh
@@ -36,7 +36,7 @@ ControllerCluster:
   CertManagerOptions:
     Release: cert-manager
     Chart: cert-manager
-    Repo: "https://raw.githubusercontent.com/kubeslice/dev-charts/gh-pages/"
+    Repo: "https://raw.githubusercontent.com/kubeslice/stable-opensource-charts/gh-pages/"
     Namespace: cert-manager
     Version: v1.7.0
     Username: ${chartuser}
@@ -44,7 +44,7 @@ ControllerCluster:
   HubChartOptions:
     Release: kubeslice-controller
     Chart: kubeslice-controller
-    Repo: "https://raw.githubusercontent.com/kubeslice/dev-charts/gh-pages/"
+    Repo: "https://raw.githubusercontent.com/kubeslice/stable-opensource-charts/gh-pages/"
     Namespace: kubeslice-controller
     Username: ${chartuser}
     Password: ${chartpassword}
@@ -59,21 +59,21 @@ WorkerClusters:
 WorkerChartOptions:
   Release: kubeslice-worker
   Chart: kubeslice-worker
-  Repo: "https://raw.githubusercontent.com/kubeslice/dev-charts/gh-pages/"
+  Repo: "https://raw.githubusercontent.com/kubeslice/stable-opensource-charts/gh-pages/"
   Namespace: kubeslice-system
   Username: ${chartuser}
   Password: ${chartpassword}
 IstioBaseChartOptions:
   Release:   "istio-base"
   Chart:     "istio-base"
-  Repo:      "https://raw.githubusercontent.com/kubeslice/dev-charts/gh-pages/"
+  Repo:      "https://raw.githubusercontent.com/kubeslice/stable-opensource-charts/gh-pages/"
   Username: ${chartuser}
   Password: ${chartpassword}
   Namespace: "istio-system"
 IstioDChartOptions:
   Release:   "istiod"
   Chart:     "istio-discovery"
-  Repo:      "https://raw.githubusercontent.com/kubeslice/dev-charts/gh-pages/"
+  Repo:      "https://raw.githubusercontent.com/kubeslice/stable-opensource-charts/gh-pages/"
   Username: ${chartuser}
   Password: ${chartpassword}
   Namespace: "istio-system"
