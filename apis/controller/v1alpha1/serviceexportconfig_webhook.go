6e0c3d2c1268989d4127c60f961659bbdabd4f6b
Priyank Upadhyay <priyank@aveshasystems.com>
2023-05-19 18:06:54 +0530

    fix(AM-8238): add serviceexport deletion webhook
    Signed-off-by: Priyank Upadhyay <priyank@aveshasystems.com>

--- apis/controller/v1alpha1/serviceexportconfig_webhook.go
+++ apis/controller/v1alpha1/serviceexportconfig_webhook.go
@@ -33,12 +33,14 @@ type customValidationServiceExport func(ctx context.Context, serviceExportConfig
 
 var customCreateValidationServiceExport func(ctx context.Context, serviceExportConfig *ServiceExportConfig) error = nil
 var customUpdateValidationServiceExport func(ctx context.Context, serviceExportConfig *ServiceExportConfig) error = nil
+var customDeleteValidationServiceExport func(ctx context.Context, serviceExportConfig *ServiceExportConfig) error = nil
 var serviceExportConfigWebhookClient client.Client
 
-func (r *ServiceExportConfig) SetupWebhookWithManager(mgr ctrl.Manager, validateCreate customValidationServiceExport, updateUpdate customValidationServiceExport) error {
+func (r *ServiceExportConfig) SetupWebhookWithManager(mgr ctrl.Manager, validateCreate customValidationServiceExport, validateUpdate customValidationServiceExport, validateDelete customValidationServiceExport) error {
 	serviceExportConfigWebhookClient = mgr.GetClient()
 	customCreateValidationServiceExport = validateCreate
-	customUpdateValidationServiceExport = updateUpdate
+	customUpdateValidationServiceExport = validateUpdate
+	customDeleteValidationServiceExport = validateDelete
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(r).
 		Complete()
@@ -46,7 +48,7 @@ func (r *ServiceExportConfig) SetupWebhookWithManager(mgr ctrl.Manager, validate
 
 // TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
-//+kubebuilder:webhook:path=/mutate-controller-kubeslice-io-v1alpha1-serviceexportconfig,mutating=true,failurePolicy=fail,sideEffects=None,groups=controller.kubeslice.io,resources=serviceexportconfigs,verbs=create;update,versions=v1alpha1,name=mserviceexportconfig.kb.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/mutate-controller-kubeslice-io-v1alpha1-serviceexportconfig,mutating=true,failurePolicy=fail,sideEffects=None,groups=controller.kubeslice.io,resources=serviceexportconfigs,verbs=create;update;delete,versions=v1alpha1,name=mserviceexportconfig.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Defaulter = &ServiceExportConfig{}
 
@@ -79,7 +81,6 @@ func (r *ServiceExportConfig) ValidateUpdate(old runtime.Object) error {
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *ServiceExportConfig) ValidateDelete() error {
 	serviceexportlog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
-	return nil
+	serviceExportConfigCtx := util.PrepareKubeSliceControllersRequestContext(context.Background(), serviceExportConfigWebhookClient, nil, "ServiceExportConfigValidation", nil)
+	return customDeleteValidationServiceExport(serviceExportConfigCtx, r)
 }
