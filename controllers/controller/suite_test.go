cc1f259ce9118a6195067ca966336713e13db9c2
Priyank Upadhyay <priyank@aveshasystems.com>
2023-06-19 20:12:35 +0530

    feat(): integration test setup + project ITs
    * fix(AM-8057): setup Integration tests
    
    Signed-off-by: Priyank Upadhyay <priyank@aveshasystems.com>
    
    * fix(): new tests
    
    * fix(): project integration tests added (#165)
    
    * fix(): project integration tests added
    
    Signed-off-by: Puran Adhikari <puran.adhikari@aveshasystems.com>
    
    * fix(): group kind
    
    Signed-off-by: Puran Adhikari <puran.adhikari@aveshasystems.com>
    
    ---------
    
    Signed-off-by: Puran Adhikari <puran.adhikari@aveshasystems.com>
    
    * fix(): removal of cluster test
    
    Signed-off-by: Puran Adhikari <puran.adhikari@aveshasystems.com>
    
    * fix(): mkefile changes
    
    Signed-off-by: Puran Adhikari <puran.adhikari@aveshasystems.com>
    
    * fix(): namespace service kind fix
    
    Signed-off-by: Puran Adhikari <puran.adhikari@aveshasystems.com>
    
    ---------
    
    Signed-off-by: Priyank Upadhyay <priyank@aveshasystems.com>
    Signed-off-by: Puran Adhikari <puran.adhikari@aveshasystems.com>
    Co-authored-by: Md Imran <imranaec@outlook.com>
    Co-authored-by: Puran Adhikari <7930721+PuranAdhikari@users.noreply.github.com>
    Co-authored-by: Puran Adhikari <puran.adhikari@aveshasystems.com>

---
+++ controllers/controller/suite_test.go
@@ -0,0 +1,189 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	workerv1alpha1 "github.com/kubeslice/kubeslice-controller/apis/worker/v1alpha1"
+	"github.com/kubeslice/kubeslice-controller/metrics"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"path/filepath"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"testing"
+	"time"
+
+	controllerv1alpha1 "github.com/kubeslice/kubeslice-controller/apis/controller/v1alpha1"
+	ossEvents "github.com/kubeslice/kubeslice-controller/events"
+
+	"github.com/kubeslice/kubeslice-controller/service"
+	"github.com/kubeslice/kubeslice-monitoring/pkg/events"
+
+	"github.com/kubeslice/kubeslice-controller/util"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	//+kubebuilder:scaffold:imports
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var (
+	cfg           *rest.Config
+	k8sClient     client.Client
+	testEnv       *envtest.Environment
+	svc           *service.Services
+	eventRecorder events.EventRecorder
+	ctx           context.Context
+	cancel        context.CancelFunc
+	controllerLog = util.NewLogger().With("name", "controllers")
+)
+
+const (
+	timeout = time.Second * 10
+	// duration = time.Second * 10
+	interval              = time.Millisecond * 250
+	controlPlaneNamespace = "kubeslice-controller"
+)
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecs(t, "Controller Suite")
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: true,
+		CRDInstallOptions: envtest.CRDInstallOptions{
+			MaxTime: 60 * time.Second,
+		},
+	}
+
+	var err error
+	// cfg is defined in this file globally.
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	err = clientgoscheme.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = controllerv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = workerv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	//+kubebuilder:scaffold:scheme
+
+	ctx, cancel = context.WithCancel(context.TODO())
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	mr := service.WithMetricsRecorder()
+	ns := service.WithNameSpaceService(mr)
+	rp := service.WithAccessControlRuleProvider()
+	acs := service.WithAccessControlService(rp, mr)
+	js := service.WithJobService()
+	wscs := service.WithWorkerSliceConfigService(mr)
+	ss := service.WithSecretService(mr)
+	wsgs := service.WithWorkerSliceGatewayService(js, wscs, ss, mr)
+	c := service.WithClusterService(ns, acs, wsgs, mr)
+	wsi := service.WithWorkerServiceImportService(mr)
+	se := service.WithServiceExportConfigService(wsi, mr)
+	wsgrs := service.WithWorkerSliceGatewayRecyclerService()
+	sc := service.WithSliceConfigService(ns, acs, wsgs, wscs, wsi, se, wsgrs, mr)
+	sqcs := service.WithSliceQoSConfigService(wscs, mr)
+	p := service.WithProjectService(ns, acs, c, sc, se, sqcs, mr)
+	svc = service.WithServices(wscs, p, c, sc, se, wsgs, wsi, sqcs, wsgrs)
+
+	service.ProjectNamespacePrefix = util.AppendHyphenAndPercentageSToString("kubeslice")
+	rbacResourcePrefix := util.AppendHyphenToString("kubeslice-rbac")
+	service.RoleBindingWorkerCluster = rbacResourcePrefix + "worker-%s"
+	service.RoleBindingReadOnlyUser = rbacResourcePrefix + "ro-%s"
+	service.RoleBindingReadWriteUser = rbacResourcePrefix + "rw-%s"
+	service.ServiceAccountWorkerCluster = rbacResourcePrefix + "worker-%s"
+	service.ServiceAccountReadOnlyUser = rbacResourcePrefix + "ro-%s"
+	service.ServiceAccountReadWriteUser = rbacResourcePrefix + "rw-%s"
+
+	controlPlaneNS := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: controlPlaneNamespace,
+		},
+	}
+	Expect(k8sClient.Create(ctx, controlPlaneNS)).Should(Succeed())
+
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	//setting up the event recorder
+	eventRecorder = events.NewEventRecorder(k8sClient, k8sManager.GetScheme(), ossEvents.EventsMap, events.EventRecorderOptions{
+		Version:   "v1alpha1",
+		Cluster:   util.ClusterController,
+		Component: util.ComponentController,
+		Slice:     util.NotApplicable,
+	})
+
+	// setting up metrics collector
+	go metrics.StartMetricsCollector(service.MetricPort, true)
+
+	err = (&ProjectReconciler{
+		Client:         k8sClient,
+		Scheme:         k8sManager.GetScheme(),
+		Log:            controllerLog.With("name", "Project"),
+		EventRecorder:  &eventRecorder,
+		ProjectService: svc.ProjectService,
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&ClusterReconciler{
+		Client:         k8sClient,
+		Scheme:         k8sManager.GetScheme(),
+		Log:            controllerLog.With("name", "Cluster"),
+		EventRecorder:  &eventRecorder,
+		ClusterService: svc.ClusterService,
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	}()
+
+})
+
+var _ = AfterSuite(func() {
+	cancel()
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+})
--- go.mod
+++ go.mod
@@ -17,6 +17,12 @@ require (
 )
 
 require (
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	golang.org/x/tools v0.9.1 // indirect
+)
+
+require (
 	cloud.google.com/go v0.81.0 // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
 	github.com/Azure/go-autorest/autorest v0.11.18 // indirect
@@ -31,11 +37,11 @@ require (
 	github.com/fatih/camelcase v1.0.0 // indirect
 	github.com/form3tech-oss/jwt-go v3.2.3+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/zapr v1.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/google/uuid v1.1.2 // indirect
@@ -47,6 +53,8 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.5
+	github.com/onsi/gomega v1.27.6
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
@@ -57,11 +65,11 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/term v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
