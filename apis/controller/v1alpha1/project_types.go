16bbf5b1ff4c71bda32d4ee61d5a8a89fc18de00
Rohan Kumar Mainali <92391187+RohanKumarMainali@users.noreply.github.com>
2025-01-10 15:54:55 +0545

    fix(): add requeueOnFailure and enableAutoEviction field in cluster types  (#237)
    * fix(): add enableAutoEviction flag in cluster spec
    
    Signed-off-by: RohanKumarMainali <rohan.manali@aveshasystems.com>
    
    * fix(): typo of omitempty in project apis
    
    Signed-off-by: RohanKumarMainali <rohan.manali@aveshasystems.com>
    
    * fix(): add requeueOnFailure field
    
    Signed-off-by: RohanKumarMainali <rohan.manali@aveshasystems.com>
    
    ---------
    
    Signed-off-by: RohanKumarMainali <rohan.manali@aveshasystems.com>

--- apis/controller/v1alpha1/project_types.go
+++ apis/controller/v1alpha1/project_types.go
@@ -25,8 +25,7 @@ type ProjectSpec struct {
 	// ServiceAccount is a field of Project. Edit project_types.go to remove/update
 	ServiceAccount ServiceAccount `json:"serviceAccount,omitempty"`
 	// If defaultSliceCreation is true, then the default slice will be created
-	//+kubebuilder:validation:Optional
-	DefaultSliceCreation bool `json:"defaultSliceCreation,omitemtpy"`
+	DefaultSliceCreation bool `json:"defaultSliceCreation,omitempty"`
 }
 
 // ServiceAccount defines the field of ProjectSpec
