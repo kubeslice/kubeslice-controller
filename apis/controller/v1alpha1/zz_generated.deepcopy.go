91bf1c74cf44e0b96f25f00f6c2b1cc83b7fe6a3
Shreesha001 <shettyshreesha552@gmail.com>
2026-07-15 23:28:13 +0530

    feat: add gateway connectivity and slice topology status fields
    Signed-off-by: Shreesha001 <shettyshreesha552@gmail.com>

--- apis/controller/v1alpha1/zz_generated.deepcopy.go
+++ apis/controller/v1alpha1/zz_generated.deepcopy.go
@@ -21,6 +21,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -787,6 +788,13 @@ func (in *SliceConfigStatus) DeepCopyInto(out *SliceConfigStatus) {
 	if in.KubesliceEvents != nil {
 		in, out := &in.KubesliceEvents, &out.KubesliceEvents
 		*out = make([]KubesliceEvent, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]v1.Condition, len(*in))
 		for i := range *in {
 			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
